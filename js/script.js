f610fe894320f7651c0170e32360ffafa616271d
polimezar <hero.twilight@mail.ru>
2020-03-13 22:41:33 +0300

    Проверка

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var link = document.querySelector(".search-button");
 var form = document.querySelector(".search-form");
 
-var adults = form.querySelector("[adults]")
-var children = form.querySelector("[children]")
-var entry = form.querySelector("[entry]")
-var departure = form.querySelector("[departure]")
+var adults = form.querySelector("[adults]");
+var children = form.querySelector("[children]");
+var entry = form.querySelector("[entry]");
+var departure = form.querySelector("[departure]");
 
 var isStorageSupport = true;
 var storageAdults = "";
@@ -18,20 +18,20 @@ try {
 }
 
 link.addEventListener("click", function(evt) {
- event.preventDefault();
- form.classList.toggle("form-hide")
+ evt.preventDefault();
+ form.classList.toggle("form-hide");
  form.classList.remove("form-error");
 });
 
 form.addEventListener("submit", function (evt) {
  if (!entry.value || !departure.value || !adults.value || !children.value) {
      evt.preventDefault();
-     forn.classList.remove("form-error");
-     forn.offsetWidth = form.offsetWidth;
-      form.classlist.add("form-error");
+     form.classList.remove("form-error");
+     form.offsetWidth = form.offsetWidth;
+      form.classList.add("form-error");
      if (!departure.value) {
          departure.focus();
-         if (!entry.value)
+         if (!entry.value);
              entry.focus();
 }
      } else if (!adults.value) {
