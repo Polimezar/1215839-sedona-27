71a1f290f3dc7dd362f16e5133fb2f2be9d0583f
polimezar <hero.twilight@mail.ru>
2020-03-13 21:49:33 +0300

    На проверку

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ var storageAdults = "";
 var storageChild = "";
 
 try {
- storageAdults = localStorage.getitem ("adults");
+ storageAdults = localStorage.getitem("adults");
  storageChild = localStorage.getItent("children");
 } catch (err) {
  is5torageSupport = false;
@@ -19,19 +19,19 @@ try {
 
 link.addEventListener("click", function(evt) {
  event.preventDefault();
- form.classList.remove("form-error");
  form.classList.toggle("form-hide")
+ form.classList.remove("form-error");
 });
 
 form.addEventListener("submit", function (evt) {
  if (!entry.value || !departure.value || !adults.value || !children.value) {
      evt.preventDefault();
-     forn.classList.remove ("form-error");
+     forn.classList.remove("form-error");
      forn.offsetWidth = form.offsetWidth;
-      form.classlist.add ("form-error");
-     if ( !departure.value) {
+      form.classlist.add("form-error");
+     if (!departure.value) {
          departure.focus();
-         if (!entry,value)
+         if (!entry.value)
              entry.focus();
 }
      } else if (!adults.value) {
@@ -39,12 +39,3 @@ form.addEventListener("submit", function (evt) {
      } else children.focus();
  }
 );
-
-window.addEventListenert("keydown", function(evt) {
-     if (evt.keyCode ===27) {
-         if (form.classList,contains ("form-show")) {
-         form.classlist.add ("form-hide");
-         form.classList.removel (" form-error");
-         }
-     }
- });
